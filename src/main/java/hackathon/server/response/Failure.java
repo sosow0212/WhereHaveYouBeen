0e1f031a95845f0d70badc9be2d0981cec794d89
sosow0212 <sosow0212@naver.com>
2022-08-23 18:53:30 +0900

    feat: Create controller response dto

---
+++ src/main/java/hackathon/server/response/Failure.java
@@ -0,0 +1,10 @@
+package hackathon.server.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Failure implements Result {
+    private String msg;
+}
---
+++ src/main/java/hackathon/server/response/Result.java
@@ -0,0 +1,4 @@
+package hackathon.server.response;
+
+public interface Result {
+}

c1ca2dffa8085f978463ee19ca2e2919fc763733
sosow0212 <sosow0212@naver.com>
2022-08-23 17:15:46 +0900

    feat: Create Project

---
+++ src/main/java/hackathon/server/WhereHaveYouBeenApplication.java
@@ -0,0 +1,13 @@
+package hackathon.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereHaveYouBeenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereHaveYouBeenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hackathon/server/WhereHaveYouBeenApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereHaveYouBeenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
